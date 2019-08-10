b3e7f893e853175afd8637d895d789b4317793c9
Alberto <muqui@hotmail.com>
2019-08-09 20:43:22 -0500

    estructura del proyecto

--- src/proyectozapopanacademyjava/main.java
+++ src/proyectozapopanacademyjava/main.java
@@ -19,7 +19,7 @@ public class main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/sceneContenedor.fxml"));
 
         Scene scene = new Scene(root);
 
